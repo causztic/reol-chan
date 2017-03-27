4f62b63b58066c1533e8c334f51785d021ec2d16
yaojie <limyaojie93@gmail.com>
2017-03-28 02:06:15 +0800

    use postgres instead of sqlite for heroku

--- migrations/001-initial-schema.sql
+++ migrations/001-initial-schema.sql
@@ -1,5 +1,5 @@
 -- Up
-CREATE TABLE Instagram (id INTEGER PRIMARY KEY, url TEXT, text TEXT, post_id TEXT UNIQUE)
+CREATE TABLE Instagram (id BIGSERIAL PRIMARY KEY, url TEXT, text TEXT, post_id TEXT UNIQUE)
 
 -- Down
 DROP TABLE Instagram
