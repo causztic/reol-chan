f72e309a139e796d213fe8b6e42ba7d21ab84ef0
causztic <limyaojie93@gmail.com>
2021-09-25 11:45:42 +0800

    Remove unused everyone config

--- config/index.ts
+++ config/index.ts
@@ -6,7 +6,6 @@ export default process.env.NODE_ENV === "production" ? {
   guildId: process.env.GUILID_ID ?? '',
   generalChannelId: process.env.GENERAL_CHANNEL_ID ?? '',
   roles: {
-    member: process.env.ROLES_MEMBER_ID ?? '',
-    everyone: process.env.ROLES_EVERYONE_ID ?? '',
+    member: process.env.ROLES_MEMBER_ID ?? ''
   }
 } : configuration;
