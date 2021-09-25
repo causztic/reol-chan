00c1cf671601fadf5d10269e4e07b69d258aed36
yaojie <limyaojie93@gmail.com>
2021-09-25 18:21:26 +0800

    fix: GUILD_ID typo

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 export default process.env.NODE_ENV === "production" ? {
   token: process.env.TOKEN ?? '',
   clientId: process.env.CLIENT_ID ?? '',
-  guildId: process.env.GUILID_ID ?? '',
+  guildId: process.env.GUILD_ID ?? '',
   generalChannelId: process.env.GENERAL_CHANNEL_ID ?? '',
   roles: {
     member: process.env.ROLES_MEMBER_ID ?? '',
