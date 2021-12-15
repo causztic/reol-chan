90f4b00ce3678c295a690d0a98653a4a71c71606
yaojie <limyaojie93@gmail.com>
2021-12-15 18:18:51 +0800

    Update tracklist

--- index.ts
+++ index.ts
@@ -8,18 +8,13 @@ import { MustBeInGuildError } from './util/mustBeInGuild';
 import { checkTwitter } from './twitter';
 
 const activities: ActivitiesOptions[] = [
-  { name: '金字塔', url: "https://open.spotify.com/track/02HNBwIheiZAuzC8p1QBPn", type: ActivityType.Listening },
-  { name: "HYPE MODE", url: "https://open.spotify.com/track/4BLp8YfoGLtspzHhN6bsi9", type: ActivityType.Listening },
-  { name: "ゆーれいずみー", url: "https://open.spotify.com/track/0NsnScldVs3A8psn3uCLZr", type: ActivityType.Listening },
-  { name: "-ムーブのための試奏曲 Nr.4-", url: "https://open.spotify.com/track/5f67BMdyuOf0VNYFFnuwRs", type: ActivityType.Listening },
-  { name: "ハーメルン", url: "https://open.spotify.com/track/76zCFsWIre89p4HQrRerbf", type: ActivityType.Listening },
-  { name: "un, deux, trois", url: "https://open.spotify.com/track/2NXqn7SaFWJmIpjwu9Ae6c", type: ActivityType.Listening },
-  { name: "insider", url: "https://open.spotify.com/track/0GY0AgfcwuKDUHaZgRv4QM", type: ActivityType.Listening },
-  { name: "ダリ", url: "https://open.spotify.com/track/7f4PaZuWlLM4ZsPTZIPLe1", type: ActivityType.Listening },
-  { name: "-ルネの小品 Nr.9-", url: "https://open.spotify.com/track/65leI2ZvSJRWtGozl5g1tf", type: ActivityType.Listening },
-  { name: "GRIMOIRE", url: "https://open.spotify.com/track/60RClClRsm1QQCXVuaRMN5", type: ActivityType.Listening },
-  { name: "1LDK", url: "https://open.spotify.com/track/27MVvs7ErNpl81dkwwNS5i", type: ActivityType.Listening },
-  { name: "白夜", url: "https://open.spotify.com/track/0n9XEQevHnxyyp4TeIb3km", type: ActivityType.Listening },
+  { name: '第六感', url: 'https://open.spotify.com/track/7JoAEFSQcQVP8CVQs9evCK', type: ActivityType.Listening },
+  { name: 'Q?', url: 'https://open.spotify.com/track/7qPd0qqWCwJcLlwh0EMzqR', type: ActivityType.Listening },
+  { name: '白夜', url: 'https://open.spotify.com/track/4uFeogGVRjhTGotbD7ibaY', type: ActivityType.Listening },
+  { name: 'Ms. CONTROL', url: 'https://open.spotify.com/track/0z0aMslUaXv7dktiOtRxt1', type: ActivityType.Listening },
+  { name: 'ミュータント', url: 'https://open.spotify.com/track/5CcIbg87TfFtajPIn4u7bD', type: ActivityType.Listening },
+  { name: 'Nd60', url: 'https://open.spotify.com/track/7ojnLCXMOYoliD4HLm4dHh', type: ActivityType.Listening },
+  { name: 'Boy', url: 'https://open.spotify.com/track/1igJtwHQhwllFr0CK34xKI?si=d9ae9bae00884dc7', type: ActivityType.Listening },
 ];
 
 const client = new Client({
